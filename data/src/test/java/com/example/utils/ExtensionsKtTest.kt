5ba53ba36d725855f7ec117e9b47f5c26c6e90ad
Volodymyr Kryvonos <kruvonosvova@gmail.com>
2022-10-24 15:44:46 +0300

    Adde tests

---
+++ data/src/test/java/com/example/utils/ExtensionsKtTest.kt
@@ -0,0 +1,14 @@
+package com.example.utils
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class ExtensionsKtTest {
+
+    @org.junit.jupiter.api.Test
+    fun parseDate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun toDate() {
+    }
+}
